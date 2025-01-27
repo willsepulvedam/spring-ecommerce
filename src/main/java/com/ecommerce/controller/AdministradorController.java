03ce7f5c13e313b0ab4b48622671c5aa9c6070b5
willsepulvedam <willmersepulveda23@gmail.com>
2025-01-27 12:40:15 -0500

    realizamos la creacion de la visualizacion de las imagenes en la vista de administrador y arreglamos el error de la base de datos de usuario_id

--- src/main/java/com/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/controller/AdministradorController.java
@@ -1,15 +1,31 @@
 package com.ecommerce.controller;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.model.Producto;
+import com.ecommerce.service.ProductoService;
+
+
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
+
+	@Autowired
+	private ProductoService productoService;
 	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos", productos);
+
 		return "administrador/home";
 	}
 }	
--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -21,8 +21,6 @@ import com.ecommerce.service.ProductoService;
 import com.ecommerce.service.UploadFileService;
 
 
-
-
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
@@ -74,14 +72,11 @@ public class ProductoController {
 	
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
 		if (file.isEmpty()) { //editamos el producto pero no cambiamos la imagen
-			Producto p = new Producto();
-			p = productoService.get(producto.getId()).get();
 			producto.setImagen(p.getImagen());
-		}else {//cuando se edita tambien la imagen
-			Producto p = new Producto();
-			p = productoService.get(producto.getId()).get();
-			
+		}else { //cuando se edita tambien la imagen
 			//eliminar no sea la imagen por defecto
 			if (!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());
@@ -89,6 +84,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
